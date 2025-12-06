d04d2e90b85aab8cb500ede7465ff8f5ce63e72b
RONNY <mboyaronny41@gmail.com>
2025-12-07 00:01:42 +0300

    stack lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,41 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = list(items)
+        self.limit = limit
+        #pass
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
+        # pass
 
     def push(self, item):
-        pass
+        if not self.full():
+            self.items.append(item)
+        #pass
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        #pass
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
+        # pass
     
     def size(self):
-        pass
+        return len(self.items)
+        # pass
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit
+        # pass
 
     def search(self, target):
-        pass
+        try:
+            index = self.items.index(target)
+            return len(self.items) - 1 - index
+        except ValueError:
+            return -1
+        # pass
